a1a74564f7219ab3f5ab7b2e5e8381626b7a94a3
chenjianhui <2294198058@qq.com>
2023-10-26 20:19:37 +0800

    feat:1.注册逻辑 2.字体规范公共css 3.路由测试 4.axios引入

--- src/app.config.js
+++ src/app.config.js
@@ -3,6 +3,24 @@ export default defineAppConfig({
     'pages/index/index',
     'pages/register/register'
   ],
+  tabBar: {
+    list: [{
+      pagePath: 'pages/index/index',
+      text: '首页',
+      iconPath:"images/index_tabbar_before.png", // 激活前的图片
+      selectedIconPath:"images/index_tabbar_before.png", // 激活后的图片
+    }, {
+      pagePath: 'pages/register/register',
+      text: '个人',
+      iconPath:"images/my_tabbar_before.png",
+      selectedIconPath:"images/my_tabbar_before.png",
+
+    }],
+    'color': '#000',
+    'selectedColor': '#56abe4',
+    'backgroundColor': '#fff',
+    'borderStyle': 'white'
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,74 @@
 <template>
-<nut-button @click="handleRegister">跳转到注册</nut-button>
+  <!--  <nut-button @click="handleRegister">跳转到注册</nut-button>-->
+  <nut-row style="height: 60px;display: flex;align-items: center;margin: 2vw;">
+    <nut-col :span="18">
+      <view style="margin-left: 5vw;">
+        <text > Hi,同学{{ br }}这么晚了，有什么心事吗？</text>
+      </view>
+    </nut-col>
+    <nut-col :span="6">
+      <view style="width: 56px;height: 56px;border-radius: 50px;background-color: aliceblue;"></view>
+    </nut-col>
+
+  </nut-row>
+
+
+  <view
+    style="height: 220px; display: flex;flex-wrap: wrap; justify-content: center; align-items: center;margin-bottom: 20px;">
+    <view class="box"></view>
+    <view class="box"></view>
+    <view class="box"></view>
+    <view class="box"></view>
+  </view>
+  <view>
+
+  <view style="display: flex;width: 100%;justify-content: center;margin-top: 20px;">
+    <text>今日推荐</text>
+    <nut-swiper :init-page="page" :pagination-visible="true" pagination-color="#426543" auto-play="3000"
+                style="width: 90%; border-radius: 15px;box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1), 4px 0px 6px rgba(0, 0, 0, 0.1);">
+      <nut-swiper-item>
+        <img src="https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg" alt=""/>
+      </nut-swiper-item>
+      <nut-swiper-item>
+        <img src="https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg" alt=""/>
+      </nut-swiper-item>
+      <nut-swiper-item>
+        <img src="https://storage.360buyimg.com/jdc-article/welcomenutui.jpg" alt=""/>
+      </nut-swiper-item>
+      <nut-swiper-item>
+        <img src="https://storage.360buyimg.com/jdc-article/fristfabu.jpg" alt=""/>
+      </nut-swiper-item>
+    </nut-swiper>
+  </view>
+  </view>
 </template>
 
 <script setup>
 import Taro from "@tarojs/taro";
+import {ref} from "vue";
 
-const handleRegister = ()=>{
+const br = ref("\n")
+const handleRegister = () => {
   Taro.navigateTo({
     url: '/pages/register/register',
   })
 }
 </script>
-<style scoped>
+<style>
+.box {
+  margin: 2vh 2vw;
+  width: 40%;
+  height: 40%;
+  background-color: gray;
+}
 
+.nut-swiper-item {
+  line-height: 500px;
+}
+
+.nut-swiper-item img {
+  width: 100%;
+  height: 100%;
+}
 
 </style>
